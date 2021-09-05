2405396f53f7cd5e1077227d920d452492e641d0
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-09-05 12:49:10 +0300

    Update set.js

--- commands/🛠️ Moderation/set.js
+++ commands/🛠️ Moderation/set.js
@@ -3,8 +3,8 @@ const guilds = require('../../data/guilds');
 
 
 module.exports = {
-  name: "set",
-  aliases: ["prefix", "set-prefix", "prefix-set"],
+  name: "gset",
+  aliases: ["gprefix", "gset-prefix", "gprefix-set"],
   execute: async(client, message, args) => {
   
   
