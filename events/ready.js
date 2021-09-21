c6752aeb31796e86eece293ad168deec193a1b3e
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-09-21 23:04:29 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 module.exports = async (client, message) => {
     
-    client.user.setActivity(`Your Commands!!! | .help FOr My Commands | Also Watching ${client.guilds.cache.size} Servers`, { type : "WATCHING" });
+    client.user.setActivity(`Your Commands!!! | !help FOr My Commands | Also Watching ${client.guilds.cache.size} Servers`, { type : "WATCHING" });
    
      /* client.ws.on('INTERACTION_CREATE', async (interaction) => {
    client.slash.commandsrun(interaction, client);
